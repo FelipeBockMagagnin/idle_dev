f5d8f0b1042e427d7789ec69636eb636cdc378e3
Felipe Magagnin <felipebmagagnin@gmail.com>
2025-11-06 21:33:27 -0300

    build: add pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import router from './router'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
+app.use(createPinia())
 app.use(router) 
 
 createApp(App).mount('#app')
